92c9f5e462e6f8e58a9c582b12c6c0e33cd1a7cd
cxpw67bq78-lang <cxpw67bq78@privaterelay.appleid.com>
2026-01-02 18:55:26 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,49 @@
+const produits = [
+
+  // 🍎 POMMES (KG)
+  { nom: "Pomme", type: "kg", image: "https://source.unsplash.com/400x300/?apple", choix: ["Pomme", "Poire", "Pêche", "Abricot"] },
+
+  // 🍐 POIRES (KG)
+  { nom: "Poire", type: "kg", image: "https://source.unsplash.com/400x300/?pear", choix: ["Poire", "Pomme", "Kiwi", "Mangue"] },
+
+  // 🍌 BANANES
+  { nom: "Banane", type: "kg", image: "https://source.unsplash.com/400x300/?banana", choix: ["Banane", "Mangue", "Papaye", "Poire"] },
+  { nom: "Banane plantain", type: "kg", image: "https://source.unsplash.com/400x300/?plantain", choix: ["Banane plantain", "Banane", "Avocat", "Mangue"] },
+
+  // 🍊 AGRUMES
+  { nom: "Orange", type: "kg", image: "https://source.unsplash.com/400x300/?orange", choix: ["Orange", "Citron", "Pamplemousse", "Mandarine"] },
+  { nom: "Citron", type: "kg", image: "https://source.unsplash.com/400x300/?lemon", choix: ["Citron", "Orange", "Kiwi", "Poire"] },
+  { nom: "Mandarine", type: "kg", image: "https://source.unsplash.com/400x300/?mandarin", choix: ["Mandarine", "Orange", "Citron", "Pêche"] },
+
+  // 🍇 RAISINS / PRUNES
+  { nom: "Raisin", type: "kg", image: "https://source.unsplash.com/400x300/?grapes", choix: ["Raisin", "Cerise", "Prune", "Fraise"] },
+  { nom: "Prune", type: "kg", image: "https://source.unsplash.com/400x300/?plum", choix: ["Prune", "Abricot", "Pêche", "Nectarine"] },
+
+  // 🍑 FRUITS D'ÉTÉ
+  { nom: "Pêche", type: "kg", image: "https://source.unsplash.com/400x300/?peach", choix: ["Pêche", "Abricot", "Nectarine", "Prune"] },
+  { nom: "Nectarine", type: "kg", image: "https://source.unsplash.com/400x300/?nectarine", choix: ["Nectarine", "Pêche", "Prune", "Abricot"] },
+  { nom: "Abricot", type: "kg", image: "https://source.unsplash.com/400x300/?apricot", choix: ["Abricot", "Pêche", "Prune", "Cerise"] },
+  { nom: "Cerise", type: "kg", image: "https://source.unsplash.com/400x300/?cherry", choix: ["Cerise", "Raisin", "Prune", "Fraise"] },
+
+  // 🥑 EXOTIQUES (PIÈCE)
+  { nom: "Avocat", type: "piece", image: "https://source.unsplash.com/400x300/?avocado", choix: ["Avocat", "Mangue", "Kiwi", "Citron"] },
+  { nom: "Mangue", type: "piece", image: "https://source.unsplash.com/400x300/?mango", choix: ["Mangue", "Papaye", "Ananas", "Pêche"] },
+  { nom: "Ananas", type: "piece", image: "https://source.unsplash.com/400x300/?pineapple", choix: ["Ananas", "Melon", "Mangue", "Papaye"] },
+  { nom: "Grenade", type: "piece", image: "https://source.unsplash.com/400x300/?pomegranate", choix: ["Grenade", "Pamplemousse", "Orange", "Tomate"] },
+  { nom: "Kiwi", type: "piece", image: "https://source.unsplash.com/400x300/?kiwi", choix: ["Kiwi", "Avocat", "Citron", "Poire"] },
+  { nom: "Noix de coco", type: "piece", image: "https://source.unsplash.com/400x300/?coconut", choix: ["Noix de coco", "Melon", "Courge", "Pastèque"] },
+
+  // 🥕 LÉGUMES KG
+  { nom: "Carotte", type: "kg", image: "https://source.unsplash.com/400x300/?carrot", choix: ["Carotte", "Navet", "Radis", "Betterave"] },
+  { nom: "Poireau", type: "kg", image: "https://source.unsplash.com/400x300/?leek", choix: ["Poireau", "Oignon", "Ciboule", "Fenouil"] },
+  { nom: "Courgette", type: "kg", image: "https://source.unsplash.com/400x300/?zucchini", choix: ["Courgette", "Concombre", "Aubergine", "Poivron"] },
+  { nom: "Aubergine", type: "kg", image: "https://source.unsplash.com/400x300/?eggplant", choix: ["Aubergine", "Courgette", "Poivron", "Tomate"] },
+  { nom: "Poivron", type: "kg", image: "https://source.unsplash.com/400x300/?bell-pepper", choix: ["Poivron", "Tomate", "Piment", "Aubergine"] },
+
+  // 🥬 LÉGUMES PIÈCE
+  { nom: "Chou", type: "piece", image: "https://source.unsplash.com/400x300/?cabbage", choix: ["Chou", "Salade", "Blette", "Épinard"] },
+  { nom: "Salade", type: "piece", image: "https://source.unsplash.com/400x300/?lettuce", choix: ["Salade", "Chou", "Épinard", "Persil"] },
+  { nom: "Concombre", type: "piece", image: "https://source.unsplash.com/400x300/?cucumber", choix: ["Concombre", "Courgette", "Poivron", "Aubergine"] },
+  { nom: "Artichaut", type: "piece", image: "https://source.unsplash.com/400x300/?artichoke", choix: ["Artichaut", "Chou-fleur", "Brocoli", "Salade"] }
+
+];
